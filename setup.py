6e59377f1f6ef63259c3557c4d98a66ae61f26d7
sklarz <sklarz@bgu.ac.il>
2019-05-01 11:29:52 +0300

    modified setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url                 = 'https://github.com/bioinfo-core-BGU/parse_blast',
     description         = 'A script for parsing tabular BLAST output',
     license             = 'Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description    =  open('README').read(),
+    long_description    =  open('README.md').read(),
     download_url        = 'https://github.com/bioinfo-core-BGU/parse_blast.git',
     platforms           = ["POSIX","Windows"],
     scripts             = ['bin/parse_blast.R',
